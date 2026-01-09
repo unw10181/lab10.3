9bbe60fd0f2101c112b7810791912c4a9f6f5e84
unw10181 <uchennanwilliams@gmail.com>
2026-01-09 00:32:55 -0500

    app.tsx imported components

--- todo-context/src/App.tsx
+++ todo-context/src/App.tsx
@@ -1,13 +1,30 @@
-
-import './App.css'
+import { TodoProvider } from "./contexts/TodoContext";
+import { FilterProvider } from "./contexts/FilterContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import FilterButtons from "./components/FilterButtons";
+import ThemeToggleButton from "./components/ThemeToggleButton";
+import "./styles/styles.css";
 
 function App() {
-
   return (
     <>
-      
+      <ThemeProvider>
+        <TodoProvider>
+          <FilterProvider>
+            <div className="app">
+              <h1>Todo App (Context API)</h1>
+              <ThemeToggleButton />
+              <TodoInput />
+              <FilterButtons />
+              <TodoList />
+            </div>
+          </FilterProvider>
+        </TodoProvider>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
